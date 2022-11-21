1d6ee1f2b22c80cec70b9650dca3bb466154c561
esposito.00@outlook.it <esposito.00@outlook.it>
2022-11-21 15:31:56 +0100

    console

---
+++ logic.js
@@ -0,0 +1,46 @@
+// Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team
+// Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
+// MILESTONE 0:
+// Creare l’array di oggetti con le informazioni fornite
+// MILESTONE 1:
+// Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
+
+// MILESTONE 2:
+// Stampare le stesse informazioni su DOM sottoforma di stringhe
+
+
+const teamMembers = [
+
+    {
+        nome: 'Wayne Barnettt',
+        ruolo: 'Founder CEO',
+        foto: 'wayne-barnett-founder-ce.jpg'
+    },
+    {
+        nome: 'Angela Caroll',
+        ruolo: 'Chief Editor',
+        foto: 'wayne-barnett-founder-ce.jpg'
+    },
+    {
+        nome: 'Walter Gordon',
+        ruolo: 'Office manager',
+        foto: 'wayne-barnett-founder-ce.jpg'
+    },
+    {
+        nome: 'Angela Lopez',
+        ruolo: 'Social media manager',
+        foto: 'wayne-barnett-founder-ce.jpg'
+    },
+    {
+        nome: 'Scott Estrada',
+        ruolo: 'Developer',
+        foto: 'wayne-barnett-founder-ce.jpg'
+    },
+    {
+        nome: 'Barbara Ramos',
+        ruolo: 'Graphic design',
+        foto: 'wayne-barnett-founder-ce.jpg'
+    }
+]
+console.log(teamMembers);
+
